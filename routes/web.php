140a15b4d0af589445b27e63a522ae554888925e
Kelpie1405 <kawasaki.m.titech@gmail.com>
2024-09-30 21:29:09 +0900

    初期化

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,3 @@ use App\Http\Controllers\GoogleMapController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::prefix('/google-map')->controller(GoogleMapController::class)->group(function () {
-//     Route::get('', 'index');
-//     Route::get('/map', 'map');
-//     Route::get('/route1', 'route1');
-//     Route::get('/route2', 'route2');
-// });
-
-Route::get('/route', [GoogleMapController::class, 'index']);
